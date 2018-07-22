e520842f1cf741cb30ef4250a232327dcc47d69f
Eduard Ryazapov <eduard.ryazapov@gmail.com>
2018-07-23 02:29:39 +0300

    delete set flash message

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,6 @@ module Users
     def facebook
       if authenticated_user.persisted?
         sign_in_and_redirect authenticated_user, event: :authentication
-        set_flash_message(:notice, :success, kind: "Facebook")
       else
         failure
       end
