b8bbec034c0bd994af003fe80b1de373f6c74664
Eduard Ryazapov <eduard.ryazapov@gmail.com>
2018-08-03 23:57:23 +0300

    update image

--- app/controllers/manage/base_controller.rb
+++ app/controllers/manage/base_controller.rb
@@ -2,9 +2,5 @@ class Manage::BaseController < ApplicationController
   include Authorization
 
   before_action :authenticate_user!
-  before_action :authorize_resource!
-
-  def authorize_resource!
-    raise NotImplementedError
-  end
+  before_action :authorize_resource
 end
